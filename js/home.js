c4d091d71ff64ee489b1e9400cab2eccc3883e3c
TechGuru <allok512566@gmail.com>
2025-09-06 20:34:20 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,14 @@
+function calcuJump(){
+    location.href="https://techguru278.github.io/Scientific-Calculator/";
+}
+
+const refersh = () =>{
+    location.href="../homePage/about/about.html"
+}
+
+const artical = () =>{
+    location.href = "https://www.extrica.com/whats-new/engineering";
+}
+const resume = () =>{
+    location.href = "./resume.html";
+}
